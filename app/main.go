f840408b4342d40e4ae7d001768d1150a7086fa8
Marauc <maraucuttance@gmail.com>
2024-03-27 11:08:52 +1000

    Button changes

--- app/main.go
+++ app/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"encoding/json"
-	"fmt"
 	"image/color"
 	"io/ioutil"
 
@@ -21,10 +20,11 @@ type Movies struct {
 type Movie struct {
 	Title string `json:"Title"`
 	Plot  string `json:"Plot"`
+	Genre string `json:"Genre"`
 }
 
 func LoadMovieData() (Movies, error) {
-	data, err := ioutil.ReadFile("./testDataMovies.json")
+	data, err := ioutil.ReadFile("./data/testDataMovies.json")
 	if err != nil {
 		return Movies{}, err
 	}
@@ -42,7 +42,7 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-	fmt.Printf("Movies: %s/n", moviesData.Results)
+	//fmt.Printf("Movies: %s/n", moviesData.Results)
 
 	a := app.New()
 	w := a.NewWindow("Treasure It Desktop")
@@ -50,11 +50,11 @@ func main() {
 	// Top Content Bar
 	tiTitle := canvas.NewText("Treasure It", color.Black)
 	tiTitle.TextSize = 40
-	searchEntry := canvas.NewText("Search", color.Black)
+	searchEntry := canvas.NewText("Home", color.Black)
 	searchEntry.TextSize = 30
-	homeButton := canvas.NewText("Home", color.Black)
+	homeButton := canvas.NewText("Collections", color.Black)
 	homeButton.TextSize = 30
-	collectionsButton := canvas.NewText("Collections", color.Black)
+	collectionsButton := canvas.NewText("Search", color.Black)
 	collectionsButton.TextSize = 30
 	TopContent := container.New(layout.NewHBoxLayout(), tiTitle, layout.NewSpacer(), searchEntry, homeButton, collectionsButton)
 	TopContentContainer := container.NewVBox(TopContent)
@@ -73,11 +73,8 @@ func main() {
 	filterIcon.FillMode = canvas.ImageFillOriginal
 	addIcon := canvas.NewImageFromFile("../images/add_icon.png")
 	addIcon.FillMode = canvas.ImageFillOriginal
-	collectionToolBar := container.New(layout.NewHBoxLayout(), typeIcon, collectionName, layout.NewSpacer(), helpIcon, searchIcon, filterIcon, addIcon)
-	collectionToolBarContainer := container.NewVBox(collectionToolBar)
-
-	// Test List
-	//var testData = []string{"Dog", "Cat", "Bear", "Horse", "Eagle", "Snake", "Tiger", "Turtle", "Frog", "Magpie", "Fox", "Zebra", "Kangaroo", "Dingo", "Giraffe", "Elephant", "Lion"}
+	//collectionToolBar := container.New(layout.NewHBoxLayout(), typeIcon, collectionName, layout.NewSpacer(), helpIcon, searchIcon, filterIcon, addIcon)
+	//collectionToolBarContainer := container.NewVBox(collectionToolBar)
 
 	// Collection List
 	collectionList := widget.NewList(
@@ -96,58 +93,59 @@ func main() {
 	editIcon.FillMode = canvas.ImageFillOriginal
 
 	// Item Tool bar
-	itemToolBar := container.New(layout.NewHBoxLayout(), layout.NewSpacer(), helpIcon, filterIcon, editIcon)
-	itemToolBarContainer := container.NewVBox(itemToolBar)
+	//itemToolBar := container.New(layout.NewHBoxLayout(), layout.NewSpacer(), helpIcon, filterIcon, editIcon)
+	//itemToolBarContainer := container.NewVBox(itemToolBar)
 
 	// Item View
-	itemTitleText := canvas.NewText("Horse", color.Black)
-	itemDescriptionText := canvas.NewText("Decription of item", color.Black)
-	horseImage := canvas.NewImageFromFile("../images/horse_image.png")
+	// itemTitleText := canvas.NewText("Horse", color.Black)
+	// itemDescriptionText := canvas.NewText("Decription of item", color.Black)
+	// horseImage := canvas.NewImageFromFile("../images/horse_image.png")
 
-	var testData2 = []string{"Name: Horsey", "Height: 213cm", "Diet: Carrot, Apple, Hay", "Birth: 12/06/2005", "Legs: 4", "Sex: Male", "Weight: 267kg", "Age: 19", "Colour: White", "Breed: Mule", "Area: 2B", "Personality: Timid", "Health: Fair", "For Sale: No"}
-
-	// Item Data List
-	itemDataList := widget.NewList(
-		func() int {
-			return len(testData2)
-		},
-		func() fyne.CanvasObject {
-			return widget.NewLabel("Template")
-		},
-		func(i widget.ListItemID, o fyne.CanvasObject) {
-			o.(*widget.Label).SetText(testData2[i])
-		})
-
-	var testData3 = []string{"Herbivore", "Fur", "Favourite", "Harmless", "Old", "Fast", "Heavy"}
+	//var testData2 = []string{"Name: Horsey", "Height: 213cm", "Diet: Carrot, Apple, Hay", "Birth: 12/06/2005", "Legs: 4", "Sex: Male", "Weight: 267kg", "Age: 19", "Colour: White", "Breed: Mule", "Area: 2B", "Personality: Timid", "Health: Fair", "For Sale: No"}
 
 	// Item Data List
-	itemTagList := widget.NewList(
-		func() int {
-			return len(testData3)
-		},
-		func() fyne.CanvasObject {
-			return widget.NewLabel("Template")
-		},
-		func(i widget.ListItemID, o fyne.CanvasObject) {
-			o.(*widget.Label).SetText(testData3[i])
-		})
+	// itemDataList := widget.NewList(
+	// 	func() int {
+	// 		return len(testData2)
+	// 	},
+	// 	func() fyne.CanvasObject {
+	// 		return widget.NewLabel("Template")
+	// 	},
+	// 	func(i widget.ListItemID, o fyne.CanvasObject) {
+	// 		o.(*widget.Label).SetText(testData2[i])
+	// 	})
 
-	itemMainInfoLayout := container.NewGridWithColumns(2,
-		(container.NewBorder(itemTitleText, nil, nil, nil, itemDescriptionText)), horseImage)
+	//var testData3 = []string{"Herbivore", "Fur", "Favourite", "Harmless", "Old", "Fast", "Heavy"}
 
-	itemMainDataLayout := container.NewGridWithColumns(2, itemDataList, itemTagList)
+	// Item Data List
+	//itemTagList := widget.NewList(
+	// func() int {
+	// 	return len(testData3)
+	// },
+	// func() fyne.CanvasObject {
+	// 	return widget.NewLabel("Template")
+	// },
+	// func(i widget.ListItemID, o fyne.CanvasObject) {
+	// 	o.(*widget.Label).SetText(testData3[i])
+	// })
 
-	itemViewLayout := container.NewBorder(itemToolBarContainer, nil, nil, nil, container.NewGridWithRows(2, itemMainInfoLayout, itemMainDataLayout))
+	//itemMainInfoLayout := container.NewGridWithColumns(2,
+	//(container.NewBorder(itemTitleText, nil, nil, nil, itemDescriptionText)), horseImage)
+
+	//itemMainDataLayout := container.NewGridWithColumns(2, itemDataList, itemTagList)
+
+	//itemViewLayout := container.NewBorder(itemToolBarContainer, nil, nil, nil, container.NewGridWithRows(2, itemMainInfoLayout, itemMainDataLayout))
 
 	itemData := widget.NewLabel("Select a movie")
-
+	itemData.Wrapping = fyne.TextWrapWord
 	// Container build
-	collectionListContainer := container.NewBorder(collectionToolBarContainer, nil, nil, nil, collectionList)
-	collectionViewLayout := container.NewGridWithColumns(2, collectionListContainer, itemViewLayout)
+	//collectionListContainer := container.NewBorder(collectionToolBarContainer, nil, nil, nil, collectionList)
+	//collectionViewLayout := container.NewGridWithColumns(2, collectionListContainer, itemViewLayout)
 
 	// Content
-	content := container.NewBorder(TopContentContainer, nil, nil, nil, collectionViewLayout)
-	content2 := container.NewHSplit(collectionList, itemData)
+	dataDisplayContainer := container.NewHSplit(collectionList, itemData)
+	dataDisplayContainer.Offset = 0.3
+	content := container.NewBorder(TopContentContainer, nil, nil, nil, dataDisplayContainer)
 
 	collectionList.OnSelected = func(id widget.ListItemID) {
 		itemData.SetText(moviesData.Results[id].Plot)
@@ -155,7 +153,6 @@ func main() {
 
 	// Setting Content to window
 	w.SetContent(content)
-	w.SetContent(content2)
 	w.Resize(fyne.NewSize(1200, 800))
 	w.ShowAndRun()
 }
